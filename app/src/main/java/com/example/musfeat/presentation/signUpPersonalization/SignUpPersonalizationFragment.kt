4747a5f7ed8000e960b502a6e8143f645ce5bb53
krasjbee <zloyjb@gmail.com>
2021-08-14 10:35:24 +0700

    feat : added circle plus drawable feat : added SignUpPersonalizationFragment.kt and its viewmodel
    Signed-off-by: krasjbee <zloyjb@gmail.com>

---
+++ app/src/main/java/com/example/musfeat/presentation/signUpPersonalization/SignUpPersonalizationFragment.kt
@@ -0,0 +1,30 @@
+package com.example.musfeat.presentation.signUpPersonalization
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.musfeat.databinding.FragmentSignUpPersonalizationBinding
+
+class SignUpPersonalizationFragment : Fragment() {
+    private var _binding: FragmentSignUpPersonalizationBinding? = null
+    private val binding get() = requireNotNull(_binding)
+    private val viewModel by viewModels<SignUpPersonalizationViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSignUpPersonalizationBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
---
+++ app/src/main/java/com/example/musfeat/presentation/signUpPersonalization/SignUpPersonalizationViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.musfeat.presentation.signUpPersonalization
+
+import androidx.lifecycle.ViewModel
+
+class SignUpPersonalizationViewModel : ViewModel()
