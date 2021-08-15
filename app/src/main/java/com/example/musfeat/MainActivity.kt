0e1f7f3371ad31ef96ddcf5cfd7bcbc1289c3cba
krasjbee <zloyjb@gmail.com>
2021-08-15 07:34:28 +0700

    fix : some textviews now cannot get out of bounds feat : added UserProfileFragment.kt and it's empty layout
    Signed-off-by: krasjbee <zloyjb@gmail.com>

--- app/src/main/java/com/example/musfeat/MainActivity.kt
+++ app/src/main/java/com/example/musfeat/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.musfeat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.musfeat.databinding.ActivityMainBinding
@@ -10,6 +11,12 @@ import dagger.hilt.android.AndroidEntryPoint
 private lateinit var binding: ActivityMainBinding
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    fun setNavBarVisibility(visibility: Boolean) {
+        binding.bottomNavBar.isVisible = visibility
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,7 +29,5 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
         val navController = navHost.navController
         binding.bottomNavBar.setupWithNavController(navController)
-
     }
-
 }
