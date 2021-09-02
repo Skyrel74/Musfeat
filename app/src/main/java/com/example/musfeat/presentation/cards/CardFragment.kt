7d1d3d1994c009851163913a8e7d39a1bb6a184b
krasjbee <zloyjb@gmail.com>
2021-09-02 16:03:59 +0700

    feat : added close button functionality for drawer side menu feat : added stub for slider interaction

--- app/src/main/java/com/example/musfeat/presentation/cards/CardFragment.kt
+++ app/src/main/java/com/example/musfeat/presentation/cards/CardFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.musfeat.R
 import com.example.musfeat.databinding.FragmentCardBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.roundToInt
 
 @AndroidEntryPoint
 class CardFragment : Fragment() {
@@ -23,6 +26,22 @@ class CardFragment : Fragment() {
         _binding = FragmentCardBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.constraintSettings.ibSettingsClose.setOnClickListener {
+            binding.root.closeDrawer(GravityCompat.START)
+        }
+        // TODO: 02.09.2021 move getting and setting radius to viewmodel , handle initial state all the following code is stub for now
+        binding.constraintSettings.tvSearchRadius.text =
+            getString(R.string.search_settings_radius, 0)
+        binding.constraintSettings.slSearchRange.addOnChangeListener { slider, value, fromUser ->
+            val roundedValue = value.roundToInt()
+            binding.constraintSettings.tvSearchRadius.text =
+                getString(R.string.search_settings_radius, roundedValue)
+        }
+
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onDestroyView() {
