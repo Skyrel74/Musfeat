8121a5d4e6f255903a7612d9f451e1b1e24e1675
krasjbee <zloyjb@gmail.com>
2021-09-02 09:31:32 +0700

    fix : removed @HiltViewModel at RestorePasswordViewModel.kt

--- app/src/main/java/com/example/musfeat/presentation/restorePassword/RestorePasswordViewModel.kt
+++ app/src/main/java/com/example/musfeat/presentation/restorePassword/RestorePasswordViewModel.kt
@@ -1,8 +1,6 @@
 package com.example.musfeat.presentation.restorePassword
 
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
-class RestorePasswordViewModel : ViewModel() {
-}
+
+class RestorePasswordViewModel : ViewModel()
